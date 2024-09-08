5d683d2797f9232b36ba64dde6798c1627d52c25
sebi <153653021+topa-mihai-sebastian@users.noreply.github.com>
2024-09-08 09:45:12 +0300

    Update stochastic_matrix.m

--- stochastic_text_gen/stochastic_matrix.m
+++ stochastic_text_gen/stochastic_matrix.m
@@ -1,8 +1,13 @@
 function retval = stochastic_matrix(k_secv_corpus, corpus_words, words_set, k_secv_set, k)
-    % build the stochastic matrix. Bonus if it is sparse
-    % The 'stochastic' matrix SHOULD NOT be scaled (divided by sum of all)
-    % It should just be entry-wise
-    % This is how the checker tests it, to not have to deal with floating point errors
+    [words_set, ~, corpus_idx] = unique(corpus_words);
+    [~, ~, k_secv_idx] = unique(k_secv_corpus);
+    retval = zeros(length(k_secv_set), length(words_set));
 
+    max_word_index = length(corpus_words) - k;
 
+    for i = 1:max_word_index
+        k_seq_index = k_secv_idx(i);
+        next_word_index = corpus_idx(i + k);
+        retval(k_seq_index, next_word_index) = retval(k_seq_index, next_word_index) + 1;
+    endfor
 endfunction
