0b9ea1934d8fd1d1505f83322e2a8aad23bd452b
sebi <153653021+topa-mihai-sebastian@users.noreply.github.com>
2024-09-08 09:53:05 +0300

    Update sample_next_word.m

--- stochastic_text_gen/sample_next_word.m
+++ stochastic_text_gen/sample_next_word.m
@@ -1,13 +1,16 @@
 function probs = sample_next_word (text, words_idx, k_secv_idx, k, stoch)
-  for i = 1:n
-    %  Probabilities next word
-    probs = sample_next_word(text, widx, kscvidx, k, stoch);
-    
-    next_word = prob_choose(probs, word_set);
-    
-    % append next word
-    text = [text, next_word];
-  endfor
+  % ultima k-secv din text
+  last_k_secv = strjoin(text(end-k+1:end), ' ');
   
-  retval = text;
+  % verifica daca exista in k_secv_idx
+  if isKey(k_secv_idx, last_k_secv)
+    % daca exista luam indicele
+    idx = k_secv_idx(last_k_secv);
+    
+    % ret prob pentru k-secv
+    probs = stoch(:, idx);
+  else
+    % daca nu exista ret zeros
+    probs = zeros(size(stoch, 1), 1);
+  endif
 endfunction
